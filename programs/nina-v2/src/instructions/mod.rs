425e949539451d72294251debed233f69f6d3fdc
facing-n <mike@ninaprotocol.com>
2025-06-20 13:24:48 -0400

    add release close ix

--- programs/nina-v2/src/instructions/mod.rs
+++ programs/nina-v2/src/instructions/mod.rs
@@ -2,8 +2,10 @@ pub mod release_init_v2;
 pub mod release_purchase;
 pub mod release_init_and_purchase;
 pub mod release_update;
+pub mod release_close;
 
 pub use release_init_v2::*;
 pub use release_purchase::*;
 pub use release_init_and_purchase::*;
 pub use release_update::*;
+pub use release_close::*;
